64e917af49775b835c4108c45cb4734a2f84e4cc
hncambraia <108847396+hncambraia@users.noreply.github.com>
2022-08-16 20:33:18 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -22,28 +22,45 @@ function titleCase(str) {
     }
     return str.join(' ');
 }
-texto = ""
-fetch('https://prod-110.westus.logic.azure.com/workflows/e50f80756b9b43baa71d055fbee3d9c6/triggers/manual/paths/invoke/tipo/Cotacao?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=MUPtVkRaZGh1maM7uzFu2cmmaebhC1aKvLcfMfhirw0', options)
-    .then(response => {
-        response.json()
+function recuperaCotacoes(){
+    texto = ""
+    fetch('https://prod-110.westus.logic.azure.com/workflows/e50f80756b9b43baa71d055fbee3d9c6/triggers/manual/paths/invoke/tipo/Cotacao?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=MUPtVkRaZGh1maM7uzFu2cmmaebhC1aKvLcfMfhirw0', options)
+        .then(response => {
+            response.json()
+    
+                .then(data => {
+                    // console.log(data)
+                    for (index = 0; index < data.value.length; index++) {
+                        dataCotacao = formataData(data.value[index].Data)
+                        texto += "Data: " + dataCotacao + " - R$ " + data.value[index].Valor + ",00 "
+    
+                    }
+                    document.getElementById("texto").innerHTML += "<marquee >" + texto + "</marquee>"
+    
+                })
+    
+        })
+        .catch(e => {
+            console.log("ERRO: " + e)
+        })
+    
+    
+}
 
-            .then(data => {
-                // console.log(data)
-                for (index = 0; index < data.value.length; index++) {
-                    dataCotacao = formataData(data.value[index].Data)
-                    texto += "Data: " + dataCotacao + " - R$ " + data.value[index].Valor + ",00 "
-
-                }
-                document.getElementById("texto").innerHTML += "<marquee >" + texto + "</marquee>"
-
-            })
-
-    })
-    .catch(e => {
-        console.log("ERRO: " + e)
-    })
 
 
+function trataListas(list){
+   var lista=""
+          
+    for (indexIngredientes = 0; indexIngredientes < list.length; indexIngredientes++) {
+        item = "<li>" + list[indexIngredientes] + "</li>"
+        lista +=item
+         
+    }
+    return lista
+}
+function recuperaReceitas(){
+    
 textoReceitas = ""
 
 fetch('https://prod-110.westus.logic.azure.com/workflows/e50f80756b9b43baa71d055fbee3d9c6/triggers/manual/paths/invoke/tipo/Receitas?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=MUPtVkRaZGh1maM7uzFu2cmmaebhC1aKvLcfMfhirw0', options)
@@ -52,39 +69,21 @@ fetch('https://prod-110.westus.logic.azure.com/workflows/e50f80756b9b43baa71d055
 
             .then(data => {
                 //console.log(data)
-                for (index = 0; index < data.value.length; index++) {
-                    var Ingredientes = []
-                    var lista=""
-                    Ingredientes = titleCase(data.value[index].Ingredientes).split("\n")
-
-             
-            
-                    for (indexIngredientes = 0; indexIngredientes < Ingredientes.length; indexIngredientes++) {
-                        item = "<li>" + Ingredientes[indexIngredientes] + "</li>"
-                        lista +=item
-                         
-                    }
-                    var modoPerparo = []
-                    var listaModoPerparo=""
-                    var itemModo=""
-                    modoPerparo = titleCase(data.value[index].ModoPreparo).split("\n")
-
-             
-            
-                    for (indexmodoPerparo = 0; indexmodoPerparo < modoPerparo.length; indexmodoPerparo++) {
-                        itemModo = "<li>" + modoPerparo[indexmodoPerparo] + "</li>"
-                        listaModoPerparo +=itemModo
-                         
-                    }
               
+                for (index = 0; index < data.value.length; index++) {
                   
+                    Ingredientes = titleCase(data.value[index].Ingredientes).split("\n")
+                    listaIngredientes = trataListas(Ingredientes)
+                                         
+                    modoPerparo = titleCase(data.value[index].ModoPreparo).split("\n")
+                    listaModos = trataListas(modoPerparo)
+                   
                     textoReceitas += "<b>Título: </b>" + titleCase(data.value[index].Titulo) +
-                        " <br><b>Ingredientes:</b> <ul>" + lista
-                        +
-
-
-                        "</ul><br> <b>Modo de Preparo:</b>"
-                        + "<ol>"+listaModoPerparo + "</ol><br> <img height='200px' width='200px' src='" + data.value[index].Imagem + "'><br>"
+                        " <br><b>Ingredientes:</b> <ul>" + listaIngredientes
+                        +"</ul><br> <b>Modo de Preparo:</b>"
+                        + "<ol>"+listaModos 
+                        + "</ol><br> <img height='200px' width='200px' src='" 
+                        + data.value[index].Imagem + "'><br>"
 
                 }
                 document.getElementById("receita").innerHTML += textoReceitas
@@ -96,3 +95,8 @@ fetch('https://prod-110.westus.logic.azure.com/workflows/e50f80756b9b43baa71d055
     .catch(e => {
         console.log("ERRO: " + e)
     })
+
+}
+
+recuperaCotacoes()
+recuperaReceitas()
